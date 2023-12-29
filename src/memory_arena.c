e559662db3700045688f181b729262cfdfabd8ee
Magí Romanyà <magiromanya@gmail.com>
2023-12-29 16:47:27 +0100

    Fixed memory arena bug

--- src/memory_arena.c
+++ src/memory_arena.c
@@ -5,7 +5,7 @@ MemoryArena InitializeMemoryArena(size_t total_capacity) {
     arena.size = 0;
     arena.data = malloc(total_capacity);
     if (!arena.data) {
-        printf("Error while initializing arena, buy more RAM lol.");
+        printf("Error while initializing arena, buy more RAM lol.\n");
         exit(1);
     }
     arena.capacity = total_capacity;
@@ -16,11 +16,11 @@ void DestroyMemoryArena(MemoryArena* arena) { free(arena->data); }
 
 void* arena_alloc(MemoryArena* arena, size_t size) {
     if (arena->size + size > arena->capacity) {
-        printf("Not enough memory in the ARENA! :(");
+        printf("Not enough memory in the ARENA! :(\n");
         return NULL;
     }
 
-    void* ptr = (char*)(arena->data) + size;
+    void* ptr = (char*)(arena->data) + arena->size;
     arena->size += size;
 
     return ptr;
